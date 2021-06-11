3516bae4bbfd31c42de554a05cabbc735d89c65a
Abdulkhakim Magomedov <thevckit@gmail.com>
2021-06-11 23:04:12 +0300

    start madina

---
+++ Madina/Confectionery/Confectionery/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Confectionery.Views.Pages.DashboardPages;
+using System;
+using System.Windows;
+
+namespace Confectionery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new MenuPage());
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            btnBack.Visibility = MainFrame.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
